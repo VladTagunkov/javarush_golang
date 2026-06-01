b3c4837bc71fa27b6ff8e540427c1fe0039c08ab
Vlad Tagunkov <developer.unity3d.com@gmail.com>
2026-06-01 21:50:51 +0200

    start_22_lvl

---
+++ core/level22/task01/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type User struct {
+	FirstName string
+	LastName  string
+}
+
+// FullName возвращает имя и фамилию в "человеческом" виде: через один пробел.
+func (u User) FullName() string {
+	// TODO: Реализуйте форматирование полного имени в виде "<FirstName> <LastName>" с ровно одним пробелом.
+	return ""
+}
+
+func main() {
+	var firstName, lastName string
+	fmt.Scan(&firstName, &lastName)
+
+	u := User{
+		FirstName: firstName,
+		LastName:  lastName,
+	}
+
+	fmt.Println(u.FullName())
+}
